d17eae0e90217e64ac865260ce5c7cbffd296b4e
Stamatis Pavlis <shamos_pv@hotmail.com>
2020-03-16 22:46:37 +0200

    chore(api): connect to mongodb

--- src/api/src/app.ts
+++ src/api/src/app.ts
@@ -1,23 +1,45 @@
 import Koa from 'koa';
 import bodyParser from 'koa-bodyparser';
 import KoaCors from 'koa-cors';
-import { getEnviromentVariables } from './core/enviroment-variables';
-import { httpErrorHandler } from './core/utils/http.utils';
+import mongoose from 'mongoose';
+import {getEnviromentVariables} from './core/enviroment-variables';
+import {httpErrorHandler} from './core/utils/http.utils';
 
 const app = new Koa();
 app.use(bodyParser());
 app.use(KoaCors());
 
+// #region Mongo
+mongoose.connect(getEnviromentVariables().mongoConnection, {
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+});
+
+const dbConn = mongoose.connection;
+dbConn.on('error', () => console.error('[MongoDB]: Connection error:'));
+dbConn.once('open', () => {
+    console.log('[MongoDB]: Connected');
+});
+
+process.on('SIGINT', () => {
+    mongoose.connection.close(() => {
+        console.log('[MongoDB]: Disconnected due to application termination');
+        process.exit(0);
+    });
+});
+// #endregion
+
 app.use(async (ctx, next) => {
-  try {
-    await next();
-  } catch (err) {
-    httpErrorHandler(err, ctx);
-  }
+    try {
+        await next();
+    } catch (err) {
+        httpErrorHandler(err, ctx);
+    }
 });
 
 app.use(async ctx => {
-  ctx.body = 'Server is up';
+    ctx.body = 'Server is up';
 });
 
 console.log('Server is listening in: ' + getEnviromentVariables().apiPort);
--- src/api/src/core/enviroment-variables.ts
+++ src/api/src/core/enviroment-variables.ts
@@ -1,5 +1,6 @@
 export const getEnviromentVariables = () => {
   return {
     apiPort: process.env.API_PORT || 3000,
+    mongoConnection: process.env.MONGO_URL || 'mongodb://127.0.0.1:27017/companyManagementDemo',
   };
 };
