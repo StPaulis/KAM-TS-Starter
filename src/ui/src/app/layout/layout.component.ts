c08305503db7fb336684ae0ddd42870c8480dcb7
Stamatis Pavlis <shamos_pv@hotmail.com>
2020-03-20 01:40:31 +0200

    style(ui): add bootstrap & style header

--- src/ui/src/app/layout/layout.component.ts
+++ src/ui/src/app/layout/layout.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
   selector: 'cmd-layout',
   templateUrl: './layout.component.html',
-  styles: []
-})
-export class LayoutComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  styles: [`
+  header {
+    background-color: var(--primary);
+    height: 33vh;
+    clip-path: polygon(0 0, 100% 0, 100% 70%, 50% 100%, 0% 70%, 0 0);
   }
-
-}
+`]
+})
+export class LayoutComponent {}
